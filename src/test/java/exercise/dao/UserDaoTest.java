3d054170593a34017162fdec95d49a0c320177ed
chanminmaxscaler <jcm04@naver.com>
2022-10-20 13:31:20 +0900

    [Revise] UserDaoTest Test Method 작성

--- src/test/java/exercise/dao/UserDaoTest.java
+++ src/test/java/exercise/dao/UserDaoTest.java
@@ -20,11 +20,34 @@ class UserDaoTest {
     @Test
     void addAndGet() {
         UserDao userDao = context.getBean("awsUserDao", UserDao.class);
-        String id = "2";
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+
+        String id = "1";
         userDao.add(new User(id, "python", "1234"));
+        assertEquals(1, userDao.getCount());
         User user = userDao.findById(id);
 
         assertEquals("python", user.getName());
         assertEquals("1234", user.getPassword());
+    }
+
+    @Test
+    void count() {
+
+        User user1 = new User("1", "Han", "1234");
+        User user2 = new User("2", "Lee", "1423");
+        User user3 = new User("3", "Kim", "1324");
+
+        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+
+        userDao.add(user1);
+        assertEquals(1, userDao.getCount());
+        userDao.add(user2);
+        assertEquals(2, userDao.getCount());
+        userDao.add(user3);
+        assertEquals(3, userDao.getCount());
     }
 }
