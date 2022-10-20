eb56184f79d9a1654cb8231bedab24ee40b12ff1
chanminmaxscaler <jcm04@naver.com>
2022-10-20 13:13:05 +0900

    first commit User UserDao

---
+++ src/main/java/exercise/dao/DaoFactory.java
@@ -0,0 +1,25 @@
+package exercise.dao;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DaoFactory {
+    @Bean
+    public UserDao awsUserDao() {
+        return new UserDao(awsConnectionMaker());
+    }
+
+    @Bean
+    public UserDao localUserDao() {
+        return new UserDao(localConnectionMaker());
+    }
+    @Bean
+    public ConnectionMaker awsConnectionMaker() {
+        return new AwsConnectionMaker();
+    }
+    @Bean
+    public ConnectionMaker localConnectionMaker() {
+        return new LocalConnectionMaker();
+    }
+}
---
+++ src/main/java/exercise/dao/UserDao.java
@@ -0,0 +1,105 @@
+package exercise.dao;
+
+import exercise.domain.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+    ConnectionMaker connection;
+
+    public UserDao(ConnectionMaker connectionMaker) {
+        connection = connectionMaker;
+    }
+
+    public void add (User user) {
+
+        try{
+            Connection conn = connection.getConnection();
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id, name, password) VALUES (?, ?, ?)");
+
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
+
+            int status = ps.executeUpdate();
+            ps.close();
+            conn.close();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public User findById(String id) {
+
+        try {
+            Connection conn = connection.getConnection();
+            PreparedStatement ps = conn.prepareStatement ("SELECT * FROM users WHERE id = ?");
+            ps.setString(1, id);
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+            rs.close();
+            ps.close();
+            conn.close();
+            return user;
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean deleteById(String id) {
+
+        try {
+            Connection conn = connection.getConnection();
+            PreparedStatement ps = conn.prepareStatement("DELETE FROM users WHERE id = ?");
+            ps.setString(1, id);
+            ps.executeUpdate();
+            ps.close();
+            conn.close();
+
+            return true;
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteAll(){
+        try {
+            Connection conn = connection.getConnection();
+            PreparedStatement ps = conn.prepareStatement("DELETE FROM users");
+            ps.executeUpdate();
+            ps.close();
+            conn.close();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public int getCount() {
+        try {
+            Connection conn = connection.getConnection();
+            PreparedStatement ps = conn.prepareStatement("SELECT COUNT(*) FROM users");
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            int count = rs.getInt(1);
+            rs.close();
+            ps.close();
+            conn.close();
+            return count;
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
